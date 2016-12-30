b2969ca7441ac61e9b134c16167d5967246e13c1
srhuevo <garoz.daniel@gmail.com>
2016-12-30 12:38:41 +0100

    sincronizacion

--- practica-final/logemul.go
+++ practica-final/logemul.go
@@ -1,27 +1,44 @@
 package main
 
 import (
-	"fmt"
+	"logiclog"
+	"sync"
+	"rand"
+	"log"
 )
 
-var chans [5]chan int
+var chans = make([]chan int, 6)
 
-func run(log string) {
+func superproceso(log string) {
+	fOut, err := os.Create(pathFile)
+	if err {
+		log.Fatalln("No se ha podido crear el fichero")
+	}
+	lc := logiclog.Create(fOut, "Proceso 1")
 	for i:= 0;; i++ {
-		fmt.Println(i, " ", log)
+		if(rand.Int() > 0.1){
+			logiclog.Log()
+		}
+		if(rand.Int() > 0.8){
+			logiclog.
+		}
 	}
 }
 
-func upProcess(log string) {
-	chans.append(make(chan int))
+func upProcess(log string, index int) {
+	chans[index] = make(chan int)
 	go run(log)
 }
 
 func main() {
-	upProcess("soy el proceso 1")
-	upProcess("soy el proceso 2")
-	upProcess("soy el proceso 3")
-	upProcess("soy el proceso 4")
-	upProcess("soy el proceso 5")
-	upProcess("soy el proceso 6")
+	upProcess("soy el proceso 1",0)
+	upProcess("soy el proceso 2",1)
+	upProcess("soy el proceso 3",2)
+	upProcess("soy el proceso 4",3)
+	upProcess("soy el proceso 5",4)
+	upProcess("soy el proceso 6",5)
+
+	wg := &sync.WaitGroup{}
+	wg.Add(1)
+	wg.Wait()
 }
